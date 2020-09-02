f11f3050c08b16f111b40dd87116ac8b522e6242
Stephanie Rinehart <rinehart@MacBook-Pro.local>
2020-09-02 14:02:36 -0400

    fix empty route crash

--- routes/authors.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let authorsCtrl = require('../controllers/authors');
-
-router.post('/books/:id/authors', authorsCtrl.create);
-
-
-
-module.exports = router;
