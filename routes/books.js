ec85cde2b386d1a04bf60a85df0126426a5b9d52
Stephanie Rinehart <rinehart@MacBook-Pro.local>
2020-09-10 10:13:27 -0400

    add protected routes

--- routes/books.js
+++ routes/books.js
@@ -3,13 +3,18 @@ var router = express.Router();
 const booksCtrl = require('../controllers/books'); 
 
 
-router.get('/', booksCtrl.index);
-router.get('/new', booksCtrl.new);
-router.get('/:id', booksCtrl.show);
-router.post('/', booksCtrl.create);
-router.get('/:id/edit', booksCtrl.edit); 
-router.put('/:id', booksCtrl.update);
-router.delete('/:id', booksCtrl.delete); 
+router.get('/', isLoggedIn, booksCtrl.index);
+router.get('/new', isLoggedIn, booksCtrl.new);
+router.get('/:id', isLoggedIn, booksCtrl.show);
+router.post('/', isLoggedIn, booksCtrl.create);
+router.get('/:id/edit', isLoggedIn, booksCtrl.edit); 
+router.put('/:id', isLoggedIn, booksCtrl.update);
+router.delete('/:id', isLoggedIn, booksCtrl.delete); 
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+}
 
 
 module.exports = router;
--- routes/readers.js
+++ routes/readers.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 let readersCtrl = require('../controllers/readers'); 
 
+router.get('/books', isLoggedIn, readersCtrl.index);
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
   res.redirect('/auth/google');
